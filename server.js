35f24ae2d4ebc44b8eeb779b26b88d12c1d1e55c
Thierry1980 <thierrydebelder@gmail.com>
2023-12-12 15:09:22 +0100

    Created and started the server

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
-console.log("Hello Wold");
+const http = require ("http")
 
-console.log("This is a console log from my server.js file")
+// This `server` will receive data.
+const server = http.createServer()
+
+server.on("request", (request, response) => {
+    // Do something here
+})
+
+// We use our server object again to add a 'listen' method to it.
+
+server.listen(3000, () => {
+    console.log("Server started on http://127.0.0.1:3000");
+})
